f18b5ab032ed053d892e40be22ca78ddae2391e3
Arnau Mora <arnyminer.z@gmail.com>
2022-07-01 01:00:32 +0200

    :sparkles: Added `App.kt`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

---
+++ app/src/main/java/com/arnyminerz/cea/app/App.kt
@@ -0,0 +1,12 @@
+package com.arnyminerz.cea.app
+
+import android.app.Application
+import timber.log.Timber
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+    }
+}
