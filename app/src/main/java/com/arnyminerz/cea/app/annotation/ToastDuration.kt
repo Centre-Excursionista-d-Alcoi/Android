009a74bf4ead17745f7fa40e34852f18e0b8ecd5
Arnau Mora <arnyminer.z@gmail.com>
2022-07-19 22:34:23 +0200

    :sparkles: Added `@ToastDuration`
    Signed-off-by: Arnau Mora <arnyminer.z@gmail.com>

---
+++ app/src/main/java/com/arnyminerz/cea/app/annotation/ToastDuration.kt
@@ -0,0 +1,7 @@
+package com.arnyminerz.cea.app.annotation
+
+import android.widget.Toast
+import androidx.annotation.IntDef
+
+@IntDef(Toast.LENGTH_SHORT, Toast.LENGTH_LONG)
+annotation class ToastDuration
